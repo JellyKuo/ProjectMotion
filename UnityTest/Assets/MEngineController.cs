26dc9b9638df5adcd0536b31cd397c58b9e4cb71
Jelly Kuo <jellykuo1234@gmail.com>
2018-12-22 00:26:02 +0800

    UnityTest add test for input

--- UnityTest/Assets/MEngineController.cs
+++ UnityTest/Assets/MEngineController.cs
@@ -11,18 +11,23 @@ public class MEngineController : MonoBehaviour
     public GameObject btnScan;
     public GameObject btnConnect;
     public GameObject btnVib;
+    public GameObject btnStartRead;
+    public GameObject btnStopRead;
     public GameObject ddDevices;
     private List<Dropdown.OptionData> ddOptions;
     private List<MotionDevice> mDevices;
     // Use this for initialization
     void Start()
     {
+        UnitySystemConsoleRedirector.Redirect();
         ddOptions = new List<Dropdown.OptionData>();
         mDevices = new List<MotionDevice>();
         engine = new MotionEngine();
         btnScan.GetComponent<Button>().onClick.AddListener(btnScan_Click);
         btnConnect.GetComponent<Button>().onClick.AddListener(btnConnect_Click);
         btnVib.GetComponent<Button>().onClick.AddListener(btnVib_Click);
+        btnStartRead.GetComponent<Button>().onClick.AddListener(btnStartRead_Click);
+        btnStopRead.GetComponent<Button>().onClick.AddListener(btnStopRead_Click);
     }
 
     async void btnScan_Click()
@@ -42,15 +47,35 @@ public class MEngineController : MonoBehaviour
         dropdown.AddOptions(ddOptions);
     }
 
-    void btnConnect_Click()
+    async void btnConnect_Click()
     {
         var dropdown = ddDevices.GetComponent<Dropdown>();
         var mDevice = mDevices[dropdown.value];
-        engine.ConnectToDevice(mDevice);
+       var state =  await engine.BeginConnectToDevice(mDevice);
+        Debug.Log("Connect result "+state);
     }
 
     void btnVib_Click()
     {
         engine.Vibrate(5);
+    }
+
+    void btnStartRead_Click()
+    {
+        engine.BeginRead();
+        var gyroUpdateDelegate = new ProjectMotion.Input.MotionInputHandler(OnGyroUpdate);
+        engine.Input.RegisterHandler(ProjectMotion.Input.Gyro.id, gyroUpdateDelegate);
+    }
+
+    void btnStopRead_Click()
+    {
+        engine.StopRead();
+        engine.Input.DeregisterHandler(ProjectMotion.Input.Gyro.id);
+    }
+
+    void OnGyroUpdate(string Json)
+    {
+        var gyro = engine.Input.DecodeInput<ProjectMotion.Input.Gyro>(Json);
+        Debug.Log($" X:{gyro.X}, Y:{gyro.Y}, Z:{gyro.Z}");
     }
 }
